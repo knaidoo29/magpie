ce88a43bc874788568fc1363ed3f02849ab1a995
Krishna Naidoo <krishna.naidoo.11@ucl.ac.uk>
2021-11-05 15:50:44 +0000

    test docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information
 
@@ -50,5 +53,3 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
-
-sphinx-apidoc -f -o docs/source magpie
