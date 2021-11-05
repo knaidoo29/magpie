63ceffa2aae63d78cab5b3043f0f9ad91600c43b
Krishna Naidoo <krishna.naidoo.11@ucl.ac.uk>
2021-11-05 16:45:48 +0000

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,8 @@ project = 'MAGPIE'
 copyright = '2021, Krishna Naidoo'
 author = 'Krishna Naidoo'
 
-release = '0.0'
-version = '0.0.1'
+release = '0.2'
+version = release+'.1'
 
 # -- General configuration
 
@@ -21,23 +21,11 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
+    'numpydoc'
 ]
 
 # Napoleon settings
-napoleon_google_docstring = False
 napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
-napoleon_preprocess_types = False
-napoleon_type_aliases = None
-napoleon_attr_annotations = True
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
