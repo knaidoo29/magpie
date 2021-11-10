3859671a15014729f770400133d170adfdd8e140
Krishna Naidoo <krishna.naidoo.11@ucl.ac.uk>
2021-11-10 23:46:20 +0000

    update

--- magpie/__init__.py
+++ magpie/__init__.py
@@ -1,10 +1,10 @@
-from . import montecarlo as mc
-from . import densegrid as dg
+#from . import montecarlo as mc
+#from . import densegrid as dg
 
 from . import coords
 from . import one_d
 from . import pixels
-from . import plots
+#from . import plots
 from . import polar
 from . import randoms
 from . import remap
