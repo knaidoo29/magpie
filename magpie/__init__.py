d010310bc20e332f77820684dea3e748d3b4aaad
Krishna Naidoo <krishna.naidoo.11@ucl.ac.uk>
2025-11-26 21:03:49 +0000

    remove src fortran and healpix dependence for magpie-lite version

--- magpie/__init__.py
+++ magpie/__init__.py
@@ -1,5 +1,5 @@
-# from . import montecarlo as mc
-# from . import densegrid as dg
+from . import montecarlo as mc
+from . import densegrid as dg
 
 from . import coords
 from . import grids
@@ -8,6 +8,6 @@ from . import pixel
 # from . import plots
 from . import polar
 from . import randoms
-from . import remap
-from . import src
+# from . import remap
+# from . import src
 from . import utils
